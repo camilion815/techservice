516e89b4adf6101691b3030b681f928d2895391b
Mário_Fortunato_808 <theberserker815@gmail.com>
2026-07-29 19:31:25 +0100

    adicionado codigo postal

--- sql/001_criar_bd.sql
+++ sql/001_criar_bd.sql
@@ -11,6 +11,7 @@ CREATE TABLE clientes (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     telefone VARCHAR(20),
+    codigo_postal VARCHAR(10),
     status TINYINT NOT NULL DEFAULT 1,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NULL,
